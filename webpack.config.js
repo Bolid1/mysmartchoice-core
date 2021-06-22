c6937ee13c0e2af4274a3c0e1c4269341ba93fd3
Bolid1 <VDVUGaD@gmail.com>
2021-06-22 20:18:47 +0300

    vue container & hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    resolve: {
-        alias: {
-            '@': path.resolve('resources/js'),
-        },
+  devServer: {
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/js'),
     },
-};
+  },
+}
