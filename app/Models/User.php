340fd14d9a0b402ac93454d327ee2f78181e9c11
Bolid1 <VDVUGaD@gmail.com>
2021-06-23 08:59:09 +0300

    IDE helpers

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,43 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id Идентификатор пользователя
+ * @property string $name Имя пользователя
+ * @property string $email Email
+ * @property Carbon|null $email_verified_at Дата, когда пользователь подтвердил свой email
+ * @property string $password Хэш пароля
+ * @property string|null $remember_token Хэш, по которому приложение сможет "вспомнить" пользователя, когда он зайдёт через много времени
+ * @property Carbon|null $created_at Дата создания записи
+ * @property Carbon|null $updated_at Дата последнего обновления записи
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static UserFactory factory(...$parameters)
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
