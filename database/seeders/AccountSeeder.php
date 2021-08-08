4e8dd03581c4841fced8e46cfa5c411f73b04d00
Bolid1 <VDVUGaD@gmail.com>
2021-08-08 12:20:57 +0300

    Accounts seeder

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Account;
+use App\Models\Firm;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
+use function var_export;
 
 class AccountSeeder extends Seeder
 {
@@ -13,7 +17,23 @@ class AccountSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
+        Firm::query()
+            ->with('accounts')
+            ->chunkById(
+                100,
+                fn(Collection $firms) => $firms->each(static function (Firm $firm) {
+                    if (! $firm->accounts_count) {
+                        Account::factory()
+                            ->count(10)
+                            ->create([
+                                'firm_id' => $firm->id,
+                            ])
+                        ;
+                    }
+                })
+            )
+        ;
     }
 }
