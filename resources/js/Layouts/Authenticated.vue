56d2dd614b597d06b584ea5785545ce21f2acc29
Bolid1 <VDVUGaD@gmail.com>
2021-09-02 00:36:27 +0300

    new Authenticated layout

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -1,252 +1,119 @@
 <template>
-  <div>
-    <div class="min-h-screen bg-gray-100">
-      <nav class="bg-white border-b border-gray-100">
-        <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex justify-between h-16">
-            <div class="flex">
-              <!-- Logo -->
-              <div class="flex-shrink-0 flex items-center">
-                <inertia-link :href="route('firms.index')">
-                  <breeze-application-logo class="block h-9 w-auto" />
-                </inertia-link>
-              </div>
+  <el-container class="max-h-screen">
+    <el-aside :width="isMenuCollapsed ? '64px' : null" class="transition-all">
+      <el-menu
+        class="min-h-screen"
+        :default-active="activeIndex"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse="isMenuCollapsed"
+      >
+        <el-menu-item @click="toggleMenuCollapsed()">
+          <i
+            :class="{
+              'el-icon-s-fold': !isMenuCollapsed,
+              'el-icon-s-unfold': isMenuCollapsed,
+            }"
+          ></i>
+          <span>App name</span>
+        </el-menu-item>
+        <el-menu-item v-for="menuItem in menuItems" :index="menuItem.route">
+          <Link :href="menuItem.href" class="block w-full">
+            <i :class="menuItem.icon"></i>
+            <span>{{ menuItem.title }}</span>
+          </Link>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
 
-              <!-- Navigation Links -->
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <breeze-nav-link
-                  :href="route('firms.index')"
-                  :active="route().current('firms.index')"
+    <el-container>
+      <el-header class="flex justify-end">
+        <el-dropdown class="mt-8">
+          <span>{{ $page.props.auth.user.name }}</span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>
+                <Link
+                  :href="this.$route('users.edit', this.$page.props.auth.user)"
+                  >Profile</Link
                 >
-                  Firms
-                </breeze-nav-link>
-              </div>
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <breeze-nav-link
-                  :href="route('integrations.index')"
-                  :active="route().current('integrations.index')"
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <Link :href="this.$route('logout')" method="post" as="button"
+                  >Log Out</Link
                 >
-                  Integrations
-                </breeze-nav-link>
-              </div>
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <breeze-nav-link
-                  :href="route('o_auth_clients.index')"
-                  :active="route().current('o_auth_clients.index')"
-                >
-                  OAuth Clients
-                </breeze-nav-link>
-              </div>
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <breeze-nav-link
-                  :href="route('oauth.tokens.index')"
-                  :active="route().current('oauth.tokens.index')"
-                >
-                  OAuth Tokens
-                </breeze-nav-link>
-              </div>
-            </div>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </el-header>
 
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-              <!-- Settings Dropdown -->
-              <div class="ml-3 relative">
-                <breeze-dropdown align="right" width="48">
-                  <template #trigger>
-                    <span class="inline-flex rounded-md">
-                      <button
-                        type="button"
-                        class="
-                          inline-flex
-                          items-center
-                          px-3
-                          py-2
-                          border border-transparent
-                          text-sm
-                          leading-4
-                          font-medium
-                          rounded-md
-                          text-gray-500
-                          bg-white
-                          hover:text-gray-700
-                          focus:outline-none
-                          transition
-                          ease-in-out
-                          duration-150
-                        "
-                      >
-                        {{ $page.props.auth.user.name }}
-
-                        <svg
-                          class="ml-2 -mr-0.5 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </button>
-                    </span>
-                  </template>
-
-                  <template #content>
-                    <breeze-dropdown-link
-                      :href="route('users.edit', $page.props.auth.user)"
-                    >
-                      Profile
-                    </breeze-dropdown-link>
-
-                    <breeze-dropdown-link
-                      :href="route('logout')"
-                      method="post"
-                      as="button"
-                    >
-                      Log Out
-                    </breeze-dropdown-link>
-                  </template>
-                </breeze-dropdown>
-              </div>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-              <button
-                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                class="
-                  inline-flex
-                  items-center
-                  justify-center
-                  p-2
-                  rounded-md
-                  text-gray-400
-                  hover:text-gray-500 hover:bg-gray-100
-                  focus:outline-none focus:bg-gray-100 focus:text-gray-500
-                  transition
-                  duration-150
-                  ease-in-out
-                "
-              >
-                <svg
-                  class="h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    :class="{
-                      hidden: showingNavigationDropdown,
-                      'inline-flex': !showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  <path
-                    :class="{
-                      hidden: !showingNavigationDropdown,
-                      'inline-flex': showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <!-- Responsive Navigation Menu -->
-        <div
-          :class="{
-            block: showingNavigationDropdown,
-            hidden: !showingNavigationDropdown,
-          }"
-          class="sm:hidden"
-        >
-          <div class="pt-2 pb-3 space-y-1">
-            <breeze-responsive-nav-link
-              :href="route('firms.index')"
-              :active="route().current('firms.index')"
-            >
-              Firms
-            </breeze-responsive-nav-link>
-          </div>
-          <div class="pt-2 pb-3 space-y-1">
-            <breeze-responsive-nav-link
-              :href="route('integrations.index')"
-              :active="route().current('integrations.index')"
-            >
-              Integrations
-            </breeze-responsive-nav-link>
-          </div>
-
-          <!-- Responsive Settings Options -->
-          <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-              <div class="font-medium text-base text-gray-800">
-                {{ $page.props.auth.user.name }}
-              </div>
-              <div class="font-medium text-sm text-gray-500">
-                {{ $page.props.auth.user.email }}
-              </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-              <breeze-responsive-nav-link
-                :href="route('users.edit', $page.props.auth.user)"
-              >
-                Profile
-              </breeze-responsive-nav-link>
-            </div>
-
-            <div class="mt-3 space-y-1">
-              <breeze-responsive-nav-link
-                :href="route('logout')"
-                method="post"
-                as="button"
-              >
-                Log Out
-              </breeze-responsive-nav-link>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <!-- Page Content -->
-      <main>
+      <el-main>
         <slot />
-      </main>
-    </div>
-  </div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-  import BreezeApplicationLogo from "@/Components/ApplicationLogo"
-  import BreezeDropdown from "@/Components/Dropdown"
-  import BreezeDropdownLink from "@/Components/DropdownLink"
-  import BreezeNavLink from "@/Components/NavLink"
-  import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink"
+  import { Link } from "@inertiajs/inertia-vue3"
+  import { defineComponent } from "vue"
 
-  export default {
-    components: {
-      BreezeApplicationLogo,
-      BreezeDropdown,
-      BreezeDropdownLink,
-      BreezeNavLink,
-      BreezeResponsiveNavLink,
-    },
+  export default defineComponent({
+    components: { Link },
 
     data() {
       return {
-        showingNavigationDropdown: false,
+        isMenuCollapsed: true,
+        menuItemsRaw: [
+          {
+            title: "Firms",
+            route: "firms.index",
+            icon: "el-icon-s-cooperation",
+          },
+          {
+            title: "Integrations",
+            route: "integrations.index",
+            icon: "el-icon-menu",
+          },
+          {
+            title: "Auth Clients",
+            route: "o_auth_clients.index",
+            icon: "el-icon-connection",
+          },
+          {
+            title: "Auth Tokens",
+            route: "oauth.tokens.index",
+            icon: "el-icon-key",
+          },
+        ],
       }
     },
-  }
+
+    computed: {
+      menuItems() {
+        return this.menuItemsRaw.map(({ title, route, icon }) => {
+          const href = this.$route(route, {}, false)
+
+          return {
+            title,
+            route,
+            icon,
+            href,
+            active: this.$page.url.startsWith(href),
+          }
+        })
+      },
+      activeIndex() {
+        return this.menuItems.find(({ active }) => active)?.route
+      },
+    },
+
+    methods: {
+      toggleMenuCollapsed() {
+        this.isMenuCollapsed = !this.isMenuCollapsed
+      },
+    },
+  })
 </script>
